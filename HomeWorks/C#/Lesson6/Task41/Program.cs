fef5c76d161d4d5f5ab4d5cb395f0900f4a10275
VaasiliyMikhaylov91 <chelchik91@mail.ru>
2022-07-10 16:34:26 +0700

    Add Lesson6

---
+++ HomeWorks/C#/Lesson6/Task41/Program.cs
@@ -0,0 +1,13 @@
+﻿void PositiveNumbersCount(int count)
+{
+    Console.Write($"Введите число, для выхода из программы введите ноль ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number != 0)
+    {
+        if (number > 0) count ++;
+       
+        Console.WriteLine($"Количество введеных чисел больше нуля {count}");
+        PositiveNumbersCount(count);
+    }
+}
+PositiveNumbersCount(0);
---
+++ HomeWorks/C#/Lesson6/Task43/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("y = k1*x + b1");
+Console.WriteLine("y = k2*x + b2");
+Console.Write("Введите k1 ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b1 ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k2 ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b2 ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+if (k1 == k2) 
+    {
+        if (b1 == b2) Console.WriteLine("Прямые совпадают");
+        else Console.WriteLine("Прямые не пересекаются");
+    }       
+else
+{
+    double xCoordination = -Convert.ToDouble(b1 - b2) / Convert.ToDouble(k1 - k2);
+    double yCoordination = xCoordination * k1 + b1;
+    Console.WriteLine($"Координаты точки пересечения прямых ({xCoordination}; {yCoordination})");
+}
